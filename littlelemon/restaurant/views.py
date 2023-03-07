d5082dbee506c8a303b5677e4bd56de901f003fd
Grishase <102784645+Grishase@users.noreply.github.com>
2023-03-07 08:48:16 -0500

    static

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# def home(request,dish):
+#     items={
+#         'falafel':"Falafel is a deep-fried ball or patty-shaped fritter in Middle Eastern cuisine made from ground chickpeas, broad beans, or both. "
+#     }
+#     description=items[dish]
+#     return HttpResponse('<h2> {dish} </h2>',description)
+def index(request):
+    return render(request,'index.html',{})
